0dba5c1e5be074dd898cf51928dee40b1dc6003c
Wouter Hollanders <1427652hollanders@zuyd.nl>
2017-03-09 15:39:31 +0100

    ALternative Mockupe App Basis

---
+++ MockupAlt/MockupAlt/MockupAlt/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MockupAlt
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_OnClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Dashboard());
+        }
+
+        private void Btn2_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn3_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn4_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn5_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
