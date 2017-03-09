182eb2341727f135a3b9fe8eba92f48dc192ff34
ment0 <1158724horndt@zuyd.nl>
2017-03-10 00:53:52 +0100

    boomstructuur picker versie

--- MockupApp/MockupApp/MockupApp/BoomStructuur.xaml.cs
+++ MockupApp/MockupApp/MockupApp/BoomStructuur.xaml.cs
@@ -15,6 +15,25 @@ namespace MockupApp
         public BoomStructuur()
         {
             InitializeComponent();
+            PartitiepatiePicker.Title = "Participatie";
+            PartitiepatiePicker.Items.Add("School");
+            PartitiepatiePicker.Items.Add("Thuis");
+            PartitiepatiePicker.Items.Add("Vrije tijd");
+
+            ActiviteitenPicker.Title = "Activiteit";
+            ActiviteitenPicker.Items.Add("Bx2018");
+            ActiviteitenPicker.Items.Add("Bx2011");
+            ActiviteitenPicker.Items.Add("Bx202");
+            
+            CasePicker.Title = "Cases";
+            CasePicker.Items.Add("Tekenen");
+            CasePicker.Items.Add("Schrijven");
+            CasePicker.Items.Add("Plakken");
+            CasePicker.Items.Add("Knippen");
+        }
+        private void BtnAdd_OnClicked(object sender, EventArgs e)
+        {
+            //Navigation.PushAsync(new BsParticipatie(clientname));
         }
     }
 }
