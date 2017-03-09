b7601fb5e2c54d9ae7d19b166e6cf4999f20d99a
ment0 <1158724horndt@zuyd.nl>
2017-03-09 18:00:44 +0100

    Oude boomstructuur komt nu na clients, cases zijn na client weggehaald ivm de boomstructuur. meeste dingen zijn gewoon weggecomment, voor hergebruik.

---
+++ MockupApp/MockupApp/MockupApp/BsParticipatie.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MockupApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BsParticipatie : ContentPage
+    {
+        //Op mijn device (Yannik, LG G5), wanneer een part is geselecteerd, pushed die de bsact page naar de "top of the navigation stack". 
+        //Als je dan terug gaat en weer op diezelfde participatie wilt klikken, is dat niet mogelijk.
+        public BsParticipatie(string clientname)
+        {
+            InitializeComponent();
+            LvParticipatie.ItemsSource = new List<string>
+            {
+                "School","Thuis","Vrije Tijd"
+            };
+        }
+        private void LvParticipatie_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedParticipation = LvParticipatie.SelectedItem.ToString();
+            Navigation.PushAsync(new BsActiviteiten(selectedParticipation));
+        }
+    }
+}
--- MockupApp/MockupApp/MockupApp/Clients_1.xaml.cs
+++ MockupApp/MockupApp/MockupApp/Clients_1.xaml.cs
@@ -9,6 +9,7 @@ namespace MockupApp
     public partial class Clients_1 : ContentPage
     {
         public string Fullname { get; set; }
+        public string clientname { get; set; }
         public List<string> Cases { get; set; }
         public int AddButtonIndex { get; set; }
 
@@ -19,25 +20,25 @@ namespace MockupApp
             AddButtonIndex = 0;
 
             InitializeComponent();
-            EntryCaseName.IsVisible = false;
+            //EntryCaseName.IsVisible = false;
 
             GenerateInfo(clientname);
-            GenerateCases();
+           // GenerateCases();
 
         }
 
-        private void GenerateCases()
-        {
+       // private void GenerateCases()
+       // {
             //LvCases.ItemsSource = new List<string>
             //{
             //    "Case 1","Case 2","Case 3","Case 4","Case 5","Case 6"
             //};
 
-            Cases = new List<string>();
-            Cases.Add("Case 1");
-            LvCases.ItemsSource = Cases;
+       //     Cases = new List<string>();
+       //     Cases.Add("Case 1");
+       //     LvCases.ItemsSource = Cases;
 
-        }
+      //  }
 
         private void GenerateInfo(string clientname)
         {
@@ -46,21 +47,22 @@ namespace MockupApp
             int index = random.Next(achternamen.Count);
             Fullname = clientname + " " + achternamen[index];
             Lbinfo.Text = "Volledige naam: " + Fullname;
-            Lbinfo2.Text = "Overige informatie: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            Lbinfo2.Text = "Testtext waarin clientgegevens staan.";
         }
 
         private void BtnAdd_OnClicked(object sender, EventArgs e)
         {
-            if (AddButtonIndex == 0)
-            {
-                EntryCaseName.IsVisible = true;
-                AddButtonIndex = 1;
-            }
-            else
-            {
-                EntryCaseName.IsVisible = false;
-                AddButtonIndex = 0;
-            }
+            Navigation.PushAsync(new BsParticipatie(clientname));
+            //if (AddButtonIndex == 0)
+            //{
+            //    EntryCaseName.IsVisible = true;
+            //    AddButtonIndex = 1;
+            //}
+            //else
+            //{
+            //    EntryCaseName.IsVisible = false;
+            //    AddButtonIndex = 0;
+            //}
         }
     }
 }
